946d40921e7c2797e25fc8c693f61f172305ea19
warleon <51892390+warleon@users.noreply.github.com>
2022-11-10 01:52:18 -0500

    aa

--- back/face.py
+++ back/face.py
@@ -1,1 +1,17 @@
 import face_recognition as fr
+import os
+import numpy as np
+from rtree import *
+
+allowedExtensions = ["jpg"]
+
+idx = index.Index()
+def addPoint(data,vec):
+	idx.insert(data,np.repeat(vec,2))
+
+def doIndexing(rootdir):
+	for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+				filename, fileExtension = os.path.splitext(file)
+				if fileExtension.lower() in allowedExtensions:
+					print(os.path.join(subdir, file))
